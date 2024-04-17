7cd557563eec6a7857be13d007ced9a2c0e6df82
Shristi Kushwaha <131538239+Shri6apr@users.noreply.github.com>
2024-04-17 14:34:55 +0530

    Updated Solution - GfG to GitHub

--- Easy/Parenthesis Checker/parenthesis-checker.cpp
+++ Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -12,38 +12,35 @@ class Solution
     bool ispar(string x)
     {
         // Your code here
-        stack<char> st;
-
-        for(int i = 0 ;i < x.length();i++){
-            if(x[i] == '{' || x[i]== '('  || x[i]== '[') {
+        //[()]{}{[()()]()}
+        //[(])
+        //obs-open br-> under that one more open br-> u w8for that inner one to finish quicly 
+        //[)]-> open br ke saath 
+        stack<char>st;
+        for(int i = 0 ; i < x.length() ;i++){
+            if(x[i] == '[' || x[i] =='{'|| x[i] == '(' ){
                 st.push(x[i]);
-                // cout<<st;
-                
             }else{
-                    if(st.empty()){
-                         return false;//extra closig brackets
-                    }
-                    char p = st.top();
-                    if((x[i]== '}' && p=='{' ) || (x[i]==')' && p=='(') || (x[i]==']' && p == '[')){
-                        st.pop();
-                    }
-                    else {
-                       return false;
-                    } 
-                
+                if(st.empty()) return false;
+                if(x[i]==']' && st.top()=='[' || x[i]==')' && st.top()=='(' || x[i] =='}' && st.top()=='{'){
+                    st.pop();
+                }else{
+                    return false;
+                }
             }
+            
+            
         }return st.empty();
+    
         
-
-
         
-
-       
-          
+        
+        
+        
+        
     }
 
 };
-
 
 //{ Driver Code Starts.
 
